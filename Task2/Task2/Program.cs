1c8f8a91100bc312297a7fc5d41610a1d79b98af
Gunel Hummatova <116768230+gunelhmmtv@users.noreply.github.com>
2024-01-17 18:41:44 +0400

    Update Program.cs

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -6,11 +6,11 @@ if( staj>=0 && staj<=2)
     Console.WriteLine("Sizin maasiniz 2000");
 
 }
-else if( staj>=3 && staj<=5)
+else if( staj>2 && staj<=5)
 {
     Console.WriteLine("Sizin maasiniz 5000");
 }
-else if( staj>=6 && staj<=10)
+else if( staj>5 && staj<=10)
 {
     Console.WriteLine("Sizin maasiniz 10000");
 }
